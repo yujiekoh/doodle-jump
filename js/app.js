575c6c5d475062e1bdf56d176c131332c41c83cf
yujiekoh <kohyujie95@gmail.com>
2021-03-08 14:49:49 +0800

    v1: stuck at animation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 // JS Data
 const platforms = [];
 const platBottom = 100;
+let gameOver = false;
+const jumpDist = 20;
+const fallDist = 20;
+let jumpTimer;
+let fallTimer;
+class Vector {
+    constructor(x, y)
+}
 
 // CSS Data
 const containerWidth = 600
@@ -27,13 +35,64 @@ const makeDoodler = () => {
     let doodlerBottom = platBottom + platformHeight;
     $doodler.css("left", `${doodlerLeft}` + `px`);
     $doodler.css("bottom", `${doodlerBottom}` + `px`);
-
+    // $doodler.addClass("jump");
+    // if ($doodler.css("bottom") === "500px") {
+    //     $doodler.removeClass("jump").addClass("fall");
+    // }
     $(".container").append($doodler);
 }
 
-const moveDoodler = (event) => {
-    if (event.key === "ArrowLeft") { // add a move-left class to $doodler. animation duration depends on how long the user holds the key down
-        $(".doodler").animate({left: "-=10"}, 10);
+const fall = () => {
+    console.log("fall");
+    // $(".doodler").switchClass("jump", "fall", 2000, "linear", jump);
+    // $(".doodler").removeClass("jump");
+    // $(".doodler").addClass("fall");
+}
+
+const jump = () => {
+    jumpTimer = setInterval(function() {
+        let doodlerBottom = parseFloat($(".doodler").css("bottom")) + jumpDist;
+        $(".doodler").css("bottom", `${doodlerBottom}` + `px`);
+    }, 200)
+
+    // $(".doodler").switchClass("fall", "jump", 2000, "linear", function() {
+    //     console.log("max");
+    //     fall();
+        // if ($(".doodler").css("bottom") > "150px") {
+        //     console.log("max");
+        //     fall();
+        // }
+    // });
+    
+    // $(".doodler").removeClass("fall");
+    // $(".doodler").addClass("jump");
+    // console.log(getComputedStyle(document.querySelector(".doodler").bottom));
+
+    // if (parseFloat($(".doodler").css("bottom")) > 250) {
+    //     console.log(parseFloat($(".doodler").css("bottom")));
+    // }
+}
+
+const moveUpDown = () => {
+    // while (gameOver === false) {
+    //     if (isJumping === false) {
+    //         jump();
+    //     } else {
+    //         fall();
+    //     }
+    // }
+        // if ((platforms[0].css("left") - platformWidth < $(".doodler").css("left") < platforms[0].css("left") + platformWidth) && 
+        //     ($(".doodler").css("bottom") === platforms[0].css("bottom") + platformHeight)) {
+        //         jump();
+        // }
+        // else if () {
+        //     // fall();
+        // }
+}
+
+const moveLeftRight = (event) => {
+    if (event.key === "ArrowLeft") {
+        $(".doodler").animate({left: "-=10"}, 10); // separate into left() & right() functions?
         console.log("left");
     } else if (event.key === "ArrowRight") {
         $(".doodler").animate({left: "+=10"}, 10);
@@ -43,8 +102,18 @@ const moveDoodler = (event) => {
 
 // Execute
 $(() => {
-    makePlatform(6);
-    makeDoodler();
-    document.onkeydown = moveDoodler;
-    // have to add onkeyup?
+        makePlatform(6);
+        makeDoodler();
+        jump();
+        // $(".doodler").on("click", function() {
+        //     $(this).animate({bottom: "+=300"}, 1000);
+        //     $(this).toggleClass("clickedFall");
+        // })
+        // $(".doodler").on("click", function() {
+        //     $(this).toggleClass("clickedJump");
+        // })
+        // $(".button").on("click", fall);
+        document.onkeydown = moveLeftRight;
+        // have to add onkeyup?
+    
 })
